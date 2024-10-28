031c7818f5357dfe2e738aa6c67ee230b22b3c71
Meraki <meeeraaakiii@users.noreply.github.com>
2024-10-28 18:17:15 -0500

    Add env var file

--- env/.example.env.sh
+++ env/.example.env.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+export SSH_USER="vagrant"
+export SSH_HOST="127.0.0.1"
+export SSH_PORT="2222"
--- src/scripts/send_ts_files.sh
+++ src/scripts/send_ts_files.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# quit on any error
+set -e
+
+source ./env/.env.sh
+
 # Define the local directory where .ts files are stored
 LOCAL_DIR="./tmp"
 # Define the remote directory and SSH information
-REMOTE_DIR="/home/vagrant/recordings"
-SSH_USER="vagrant"
-SSH_HOST="127.0.0.1"
-SSH_PORT="2222"
+REMOTE_DIR="~/recordings"
 
+ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "mkdir -p $REMOTE_DIR"
 # Monitor the directory for new .ts files
 inotifywait -m -e create --format "%f" "$LOCAL_DIR" | while read NEW_FILE
 do
