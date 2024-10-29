7d1e5da5300301a75a60c524b9b319941faf2b03
Meraki <meeeraaakiii@users.noreply.github.com>
2024-10-28 19:36:20 -0500

    Add record_camera_and_sound.sh script

---
+++ src/scripts/record_camera_and_sound.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ffmpeg \
+-f v4l2 \
+-framerate 10 \
+-video_size 1280x720 \
+-i /dev/video0 \
+-f pulse -i alsa_input.pci-0000_00_1f.3.analog-stereo \
+-vf "drawtext=fontfile=/path/to/font.ttf:text='%{localtime\:%X}':x=10:y=10:fontsize=24:fontcolor=white:box=1:boxcolor=black@0.5" \
+-c:v libx264 -preset ultrafast -c:a aac -b:a 128k \
+-f hls \
+-hls_list_size 9999 \
+-hls_flags delete_segments+program_date_time \
+-hls_allow_cache 0 \
+-hls_segment_filename "./tmp/segment_%04d.ts" \
+'./tmp/stream.m3u8'
