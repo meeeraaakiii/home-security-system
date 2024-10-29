acc35d1b6a658e67fb61da1b63232c0a825715f9
Meraki <meeeraaakiii@users.noreply.github.com>
2024-10-28 20:54:23 -0500

    Fix cleanup script

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -9,10 +9,12 @@ fi
 
 
 if [[ $WHAT_TO_CLEAN == "server" ]]; then
+    echo "Cleaning up remote dir: $REMOTE_RECORDINGS_DIR"
     ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "rm -rf $REMOTE_RECORDINGS_DIR"
 else
+    echo "Cleaning up local dir: $LOCAL_RECORDINGS_DIR"
     # Attempt to delete the files
-    rm "$LOCAL_RECORDINGS_DIR/segment*" 2>/dev/null
+    rm $LOCAL_RECORDINGS_DIR/segment* 2>/dev/null
     # Check if the rm command was successful
     if [[ $? -eq 0 ]]; then
         echo "Deleted segments"
